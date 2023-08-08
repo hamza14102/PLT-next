138db3a00c6e2478788f0b3a2d73ebf636ad7f51
Hamza Husain <mhusain3@illinois.edu>
2023-08-08 23:49:22 +0530

    added put

--- apiHelpers/api-wrappers.js
+++ apiHelpers/api-wrappers.js
@@ -58,3 +58,33 @@ export async function deleteFromProductsByID(_id) {
     // check status code and return accordingly
     return data;
 }
+
+export async function putToProductsByID(_id, product) {
+    const url = `https://kbet2pop50.execute-api.us-east-2.amazonaws.com/default/ProductsAPILambda/`;
+    const params = new URLSearchParams({
+        TableName: 'Products',
+    });
+    const response = await fetch(`${url}?${params}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            "Key": {
+                "_id": _id,
+            },
+            "UpdateExpression": "set product_name = :n, product_description = :d, product_price = :p, product_quantity = :q",
+            "ExpressionAttributeValues": {
+                ":n": product.product_name,
+                ":d": product.product_description,
+                ":p": product.product_price,
+                ":q": product.product_quantity,
+            },
+            "ReturnValues": "UPDATED_NEW"
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+        },
+    });
+    const data = await response.json();
+    // check status code and return accordingly
+    return data;
+}
