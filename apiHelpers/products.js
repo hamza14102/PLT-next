623c36e1d6cfc18d68e69a891242c7113cd39eb4
Hamza Husain <mhusain3@illinois.edu>
2023-08-09 00:48:55 +0530

    added seach by assigned user

--- apiHelpers/products.js
+++ apiHelpers/products.js
@@ -2,6 +2,7 @@ export async function getFromProductsByID(_id) {
     const url = `https://kbet2pop50.execute-api.us-east-2.amazonaws.com/default/ProductsAPILambda/`;
     const params = new URLSearchParams({
         TableName: 'Products',
+        search_by: '_id',
         _id: _id,
     });
     const response = await fetch(`${url}?${params}`, {
@@ -85,4 +86,44 @@ export async function putToProductsByID(_id, product) {
     const data = await response.json();
     // check status code and return accordingly
     return data;
+}
+
+export async function getFromProductsByAssignedUser(user_id) {
+    const url = `https://kbet2pop50.execute-api.us-east-2.amazonaws.com/default/ProductsAPILambda/`;
+    const params = new URLSearchParams({
+        TableName: 'Products',
+        search_by: 'user_id',
+        user_id: user_id,
+    });
+    const response = await fetch(`${url}?${params}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+        },
+    });
+    const data = await response.json();
+    // check status code and return accordingly
+    const new_data = data['Items'];
+    return new_data;
+}
+
+export async function getFromProductsBySpecifiedAttribute(attribute, value) {
+    const url = `https://kbet2pop50.execute-api.us-east-2.amazonaws.com/default/ProductsAPILambda/`;
+    const params = new URLSearchParams({
+        TableName: 'Products',
+        search_by: attribute,
+        attribute: value,
+    });
+    const response = await fetch(`${url}?${params}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+        },
+    });
+    const data = await response.json();
+    // check status code and return accordingly
+    const new_data = data['Items'];
+    return new_data;
 }
