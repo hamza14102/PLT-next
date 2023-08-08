07c926e972c3a67fc6a3db76590e05dca3f5ccdc
Hamza Husain <mhusain3@illinois.edu>
2023-08-08 15:40:30 +0530

    trying to add post

--- apiHelpers/api-wrappers.js
+++ apiHelpers/api-wrappers.js
@@ -1,3 +1,5 @@
+import { func } from "prop-types";
+
 export async function getFromProductsByID(_id) {
     const url = `https://kbet2pop50.execute-api.us-east-2.amazonaws.com/default/ProductsAPILambda/`;
     const params = new URLSearchParams({
@@ -20,6 +22,9 @@ export async function getFromProductsByID(_id) {
 export async function postToProducts(product) {
     const url = `https://kbet2pop50.execute-api.us-east-2.amazonaws.com/default/ProductsAPILambda/`;
     // change product to Item which is made of AttributeValue objects (S, N, L, M, BOOL, NULL, etc.)
+    // do recursive function to change all values to AttributeValue objects
+
+    // all attribute values accepted by dynamodb
     const attributeMap = {
         'string': 'S',
         'number': 'N',
@@ -27,13 +32,25 @@ export async function postToProducts(product) {
         'map': 'M',
         'boolean': 'BOOL',
         'null': 'NULL',
+        'binary': 'B',
+        'stringSet': 'SS',
+        'numberSet': 'NS',
+        'binarySet': 'BS',
     };
 
-    const newProduct = {};
-    Object.keys(product).forEach((key) => {
-        newProduct[key] = { [attributeMap[typeof (product[key])]]: product[key] };
-    });
+    function recursiveChange(obj) {
+        const newObj = {};
+        Object.keys(obj).forEach((key) => {
+            if (typeof (obj[key]) === 'object') {
+                newObj[key] = { [attributeMap[typeof (obj[key])]]: recursiveChange(obj[key]) };
+            } else {
+                newObj[key] = { [attributeMap[typeof (obj[key])]]: obj[key] };
+            }
+        });
+        return newObj;
+    }
 
+    const newProduct = recursiveChange(product);
 
     const response = await fetch(url, {
         method: 'POST',
