e8c14394aac448b00346703cd1a9179e934e6dbd
Hamza Husain <mhusain3@illinois.edu>
2023-08-09 18:28:27 +0530

    trying to with public url

--- pages/_app.js
+++ pages/_app.js
@@ -229,7 +229,7 @@ function MyApp({
         <AuthProvider>
           <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="manifest" href="/manifest.json" />
+            <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
             <link rel="shortcut icon" href={favicon.src} />
             <link rel="apple-touch-icon" sizes="76x76" href={appleIcon.src} />
             <title>Production AI | Dashboard</title>
