cf0fc3b725b5305874e10fcf22e653962db22f79
Hamza Husain <mhusain3@illinois.edu>
2023-08-22 12:02:32 +0530

    added chekboxes for item pfc

--- pagesComponents/ecommerce/products/new-product/components/Process/index.js
+++ pagesComponents/ecommerce/products/new-product/components/Process/index.js
@@ -1,118 +1,70 @@
-/**
-=========================================================
-* NextJS Material Dashboard 2 PRO - v2.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/nextjs-material-dashboard-pro
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-// @mui material components
-import Grid from "@mui/material/Grid";
-
-// NextJS Material Dashboard 2 PRO components
-import MDBox from "/components/MDBox";
-import MDTypography from "/components/MDTypography";
-
-// NewProduct page components
-import FormField from "/pagesComponents/ecommerce/products/new-product/components/FormField";
-
-import { useEffect, useState } from "react";
-import MDInput from "/components/MDInput";
-
+import React, { useEffect, useState } from 'react';
+import { Button, Checkbox, FormControlLabel, Grid } from '@mui/material';
+import MDBox from '/components/MDBox';
+import MDTypography from '/components/MDTypography';
 
 function Process({ formData }) {
-  const [processes, setProcesses] = useState([]);
-  const [departments, setDepartments] = useState([]);
-  const [numProcesses, setNumProcesses] = useState([0]);
-  const [numDepartments, setNumDepartments] = useState(0);
+  const categories = [
+    ['Mach Shop', 'Fabrication', 'Finish', 'Outsourced', 'Others'],
+    ['Mach Shop', 'Fabrication', 'Finish', 'Outsourced', 'Others'],
+    ['Bandsaw', 'Mach Shop', 'Fabrication', 'Finish', 'Outsourced', 'Others'],
+    ['Fitting', 'Final Packing'],
+    ['Marble'],
+    ['Fabric']
+  ];
 
-  const { formField, values, errors, touched } = formData;
-  const { password } =
-    formField;
-  const {
-    password: passwordV,
-  } = values;
+  const departmentNames = ["Metal - I", "Metal - II", "Wood", "Packing", "Marble", "Fabric"];
 
-  // set passwordV to be processes every time processes changes
-  // !!! USE THIS LOGIC FOR AUTOCOMPLETE AND TAGS !!!
+  const [checked, setChecked] = useState(categories.map((category) => category.map(() => false)));
+
+  const handleCheckboxChange = (event, categoryIndex, checkboxIndex) => {
+    const newChecked = [...checked];
+    newChecked[categoryIndex][checkboxIndex] = event.target.checked;
+    setChecked(newChecked);
+  };
+
+  const handleGroupCheckboxChange = (event, categoryIndex) => {
+    const newChecked = [...checked];
+    newChecked[categoryIndex] = newChecked[categoryIndex].map(() => event.target.checked);
+    setChecked(newChecked);
+  };
+
+  const isGroupChecked = (categoryIndex) => {
+    return checked[categoryIndex].every((checkbox) => checkbox);
+  };
+
+  const isGroupIndeterminate = (categoryIndex) => {
+    return checked[categoryIndex].some((checkbox) => checkbox);
+  };
+
+  const alertChecked = () => {
+
+    const listOfChecked = checked.map((category, categoryIndex) => {
+      const groupChecked = category.some((checkbox) => checkbox);
+      const groupCheckboxes = category
+        .map((checkbox, checkboxIndex) => {
+          return {
+            name: categories[categoryIndex][checkboxIndex],
+            checked: checkbox
+          };
+        })
+        .filter((checkbox) => checkbox.checked);
+      const checkedNames = [
+        ...(groupChecked ? [`${departmentNames[categoryIndex]}`] : []),
+        ...groupCheckboxes.map((checkbox) => checkbox.name)
+      ];
+      return [...new Set(checkedNames)];
+    });
+    console.log(listOfChecked);
+    // console.log(listOfChecked);
+    // alert(listOfChecked);
+  };
+
   useEffect(() => {
-    // convert processes to string
-    if (processes.length === 0) {
-      return;
-    }
-    values.password = JSON.stringify(processes);
-    // console.log(values.password);
-  }, [processes]);
 
+    // formData.process = checked;
+  }, [checked]);
 
-  const handleProcessNameChange = (departmentIndex, processIndex, name) => {
-    setProcesses((prevState) => {
-      const newProcesses = [...prevState];
-      if (!newProcesses[departmentIndex]) {
-        newProcesses[departmentIndex] = { name: departments[departmentIndex], processes: [] };
-      }
-      newProcesses[departmentIndex].processes[processIndex].name = name;
-      return newProcesses;
-    });
-  };
-
-  const handleProcessTimeChange = (departmentIndex, processIndex, time) => {
-    setProcesses((prevState) => {
-      const newProcesses = [...prevState];
-      if (!newProcesses[departmentIndex]) {
-        newProcesses[departmentIndex] = { name: departments[departmentIndex], processes: [] };
-      }
-      newProcesses[departmentIndex].processes[processIndex].time = time;
-      return newProcesses;
-    });
-  };
-
-  const handleDepartmentProcessesChange = (departmentIndex, numProcesses) => {
-    setProcesses((prevState) => {
-      const newProcesses = [...prevState];
-      const currentNumProcesses = newProcesses[departmentIndex]?.processes?.length || 0;
-      if (numProcesses > currentNumProcesses) {
-        if (!newProcesses[departmentIndex]) {
-          newProcesses[departmentIndex] = { name: departments[departmentIndex], processes: [] };
-        }
-        for (let i = currentNumProcesses; i < numProcesses; i++) {
-          newProcesses[departmentIndex].processes.push({ name: "", time: "" });
-        }
-      } else if (numProcesses < currentNumProcesses) {
-        newProcesses[departmentIndex]?.processes?.splice(numProcesses, currentNumProcesses - numProcesses);
-      }
-      return newProcesses;
-    });
-    setNumProcesses((prevState) => {
-      const newNumProcesses = [...prevState];
-      newNumProcesses[departmentIndex] = numProcesses;
-      return newNumProcesses;
-    });
-  };
-
-  const handleDepartmentNameChange = (departmentIndex, name) => {
-    setDepartments((prevState) => {
-      const newDepartments = [...prevState];
-      newDepartments[departmentIndex] = name;
-      return newDepartments;
-    });
-    setProcesses((prevState) => {
-      const newProcesses = [...prevState];
-      if (!newProcesses[departmentIndex]) {
-        newProcesses[departmentIndex] = { name: name, processes: [] };
-      } else {
-        newProcesses[departmentIndex].name = name;
-      }
-      return newProcesses;
-    });
-  };
 
   return (
     <MDBox>
@@ -120,92 +72,37 @@ function Process({ formData }) {
         Process
       </MDTypography>
       <MDBox mt={2}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <MDInput
-              required
-              fullWidth
-              type="number"
-              label="Number of Departments"
-              value={numDepartments}
-              onChange={(e) => {
-                // ensure numDepartments is positive and less than 6
-
-                const newNumDepartments = Math.min(Math.max(parseInt(e.target.value), 0), 6);
-                setNumDepartments(newNumDepartments);
-                setDepartments((prevState) => {
-                  const newDepartments = [...prevState];
-                  if (newNumDepartments > newDepartments.length) {
-                    for (let i = newDepartments.length; i < newNumDepartments; i++) {
-                      newDepartments.push("");
-                    }
-                  } else if (newNumDepartments < newDepartments.length) {
-                    newDepartments.splice(newNumDepartments, newDepartments.length - newNumDepartments);
-                  }
-                  return newDepartments;
-                });
-                setNumProcesses((prevState) => {
-                  const newNumProcesses = [...prevState];
-                  if (newNumDepartments > newNumProcesses.length) {
-                    for (let i = newNumProcesses.length; i < newNumDepartments; i++) {
-                      newNumProcesses.push(0);
-                    }
-                  } else if (newNumDepartments < newNumProcesses.length) {
-                    newNumProcesses.splice(newNumDepartments, newNumProcesses.length - newNumDepartments);
-                  }
-                  return newNumProcesses;
-                });
-              }}
-            />
-          </Grid>
-          {departments.map((department, i) => (
-            <Grid item xs={12} key={i}>
-              <MDBox mb={2}>
-                <MDInput
-                  type="text"
-                  label={`Department Name ${i + 1}`}
-                  value={department}
-                  onChange={(e) => handleDepartmentNameChange(i, e.target.value)}
-                  required
-                />
-              </MDBox>
-              <MDInput
-                fullWidth
-                type="number"
-                label={`Number of Processes for ${department} Department`}
-                value={numProcesses[i]}
-                onChange={(e) => handleDepartmentProcessesChange(i, Math.min(Math.max(parseInt(e.target.value), 1), 15))}
-                required
+        <Grid container spacing={0}>
+          {categories.map((category, categoryIndex) => (
+            <Grid item xs={12 / categories.length} key={categoryIndex}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={isGroupIndeterminate(categoryIndex)}
+                    indeterminate={isGroupIndeterminate(categoryIndex)}
+                    onChange={(event) => handleGroupCheckboxChange(event, categoryIndex)}
+                    name={`${departmentNames[categoryIndex]}`}
+                  />
+                }
+                label={`${departmentNames[categoryIndex]}`}
               />
-              {Array.from({ length: numProcesses[i] }).map((_, j) => (
-                <Grid container spacing={2} key={j} mt={1}>
-
-                  <Grid item xs={6}>
-                    <MDInput
-                      fullWidth
-                      type="text"
-                      label={`Process Name for ${department} Department, Process ${j + 1}`}
-                      value={processes[i]?.processes?.[j]?.name || ""}
-                      onChange={(e) => handleProcessNameChange(i, j, e.target.value)}
-                      required
+              {category.map((checkboxLabel, checkboxIndex) => (
+                <FormControlLabel
+                  key={checkboxIndex}
+                  control={
+                    <Checkbox
+                      checked={checked[categoryIndex][checkboxIndex]}
+                      onChange={(event) => handleCheckboxChange(event, categoryIndex, checkboxIndex)}
+                      name={`Category ${categoryIndex + 1} Checkbox ${checkboxIndex + 1}`}
                     />
-                  </Grid>
-                  <Grid item xs={6}>
-                    <MDInput
-                      fullWidth
-                      type="text"
-                      label={`Process Time for ${department} Department, Process ${j + 1}`}
-                      value={processes[i]?.processes?.[j]?.time || ""}
-                      onChange={(e) => handleProcessTimeChange(i, j, e.target.value)}
-                      required
-                    />
-                  </Grid>
-
-                </Grid>
+                  }
+                  label={checkboxLabel}
+                />
               ))}
             </Grid>
           ))}
         </Grid>
+        {/* <Button onClick={alertChecked}>Check</Button> */}
       </MDBox>
     </MDBox>
   );
